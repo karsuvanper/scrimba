7e5fc75337a705c3bc2a5bbaf48c89d29708f5e2
karsuvanper <Karthikeyanperiyasamy01@gmail.com>
2026-02-19 14:04:53 +0530

    Update urls.py
    shout out for fellowa

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import echo  # Check if it is 'echo' or 'echo_view'
-
+#opening path for ux call
 urlpatterns = [
     path("echo/", echo), # No 'api/' here, it's added by the main file
 ]
